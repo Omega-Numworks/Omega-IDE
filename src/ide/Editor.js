28e258dbba475f0976ddb3af01ad95c2e272fbec
Quentin Guidée <quentin.guidee@gmail.com>
2020-08-13 08:21:18 +0200

    [editor] Remove useless import

--- src/ide/Editor.js
+++ src/ide/Editor.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link, Redirect} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import '../sass/omega.ide.sass'
 
 import File from './components/File';
